f3a7644eb5c2f14206faa84df4b324f57195a2dd
veronicadev <viarengoveronica@gmail.com>
2021-04-28 18:07:50 +0200

    Github issues exporter

--- src/main/java/ConfigurationReader.java
+++ src/main/java/ConfigurationReader.java
@@ -1,15 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.util.*;
 
 public class ConfigurationReader {
     private HashMap<String, String> configuration;
     private InputStream inputStream;
-
+    private static Logger logger = LoggerFactory.getLogger(ConfigurationReader.class);
     private void readConfiguration() throws IOException {
         try {
             Properties prop = new Properties();
             String propFileName = "config.properties";
-
+            logger.info("Reading config.properties - Start");
             inputStream = ConfigurationReader.class.getClassLoader().getResourceAsStream(propFileName);
 
             if (inputStream != null) {
@@ -21,13 +24,14 @@ public class ConfigurationReader {
             configuration.put("GITHUB_URL", prop.getProperty("github_url"));
             configuration.put("REPO", prop.getProperty("repo"));
             configuration.put("OWNER", prop.getProperty("owner"));
-
-            System.out.println(configuration.toString());
+            configuration.put("EXPORT_PATH", prop.getProperty("export_path"));
+            logger.info(configuration.toString());
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         } finally {
             inputStream.close();
         }
+        logger.info("Reading config.properties - End");
     }
 
     public HashMap<String, String> getConfiguration() throws IOException {
--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -1,4 +1,14 @@
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -6,32 +16,102 @@ import java.util.HashMap;
 
 public class Exporter {
     private static HashMap<String, String> configuration;
-
+    private static final Logger logger = LoggerFactory.getLogger(Exporter.class);
     public static void main (String []args) {
         try{
+            logger.info("Github issues exporter - Start");
             ConfigurationReader configurator = new ConfigurationReader();
             configuration = configurator.getConfiguration();
-
-            StringBuilder str = new StringBuilder(configuration.get("GITHUB_URL"));
-            str.append("/")
-                    .append(configuration.get("OWNER"))
-                    .append("/")
-                    .append(configuration.get("REPO"))
-                    .append("/issues");
-            URL url = new URL(str.toString());
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            in.close();
+            getIssues();
+            logger.info("Github issues exporter - End");
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
 
+    private static void getIssues() throws IOException {
+        BufferedReader in = null;
+        try{
+            /*BUILD REQUEST*/
+            logger.info("Exporter building request");
+            String str = configuration.get("GITHUB_URL") + "/" +
+                    configuration.get("OWNER") +
+                    "/" +
+                    configuration.get("REPO") +
+                    "/issues";
+            URL url = new URL(str);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            /*READING RESPONSE CONTENT*/
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                stringBuilder.append(inputLine);
+
+            String result = stringBuilder.toString();
+            logger.info("Exporter response" + result);
+            saveIssues(new JSONArray(result));
+
+        }catch (IOException e) {
+            e.printStackTrace();
+            logger.error("** Caught IOException in Exporter.getIssues **", e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("** Caught Exception in Exporter.getIssues **", e);
+        }
+        finally {
+            if(in!=null){
+                in.close();
+            }
+        }
+    }
+
+    private static void saveIssues(JSONArray issuesObj){
+        try{
+            logger.info("Saving issues in file xls");
+            if(!issuesObj.isEmpty()){
+                HSSFWorkbook workbook = new HSSFWorkbook();
+                HSSFSheet sheet = workbook.createSheet("Issues");
+                HSSFRow rowHead = sheet.createRow((short)0);
+                rowHead.createCell(0).setCellValue("N.");
+                rowHead.createCell(1).setCellValue("Title");
+                rowHead.createCell(2).setCellValue("Description");
+                rowHead.createCell(3).setCellValue("Labels");
+                rowHead.createCell(4).setCellValue("State");
+                rowHead.createCell(5).setCellValue("Assignees");
+                rowHead.createCell(6).setCellValue("Created at");
+                rowHead.createCell(7).setCellValue("Updated at");
+                rowHead.createCell(8).setCellValue("Closed at");
+                for (int i=0; i<issuesObj.length(); i++) {
+                    HSSFRow row = sheet.createRow((short)i+1);
+                    JSONObject issueObj = (JSONObject) issuesObj.get(i);
+                    row.createCell(0).setCellValue(issueObj.getLong("number"));
+                    row.createCell(1).setCellValue(issueObj.getString("title"));
+                    if(issueObj.has("body") && !issueObj.isNull("body")){
+                        row.createCell(2).setCellValue(issueObj.getString("body"));
+                    }
+                    row.createCell(3).setCellValue("Labels");
+                    row.createCell(4).setCellValue(issueObj.getString("state"));
+                    row.createCell(5).setCellValue("Assignees");
+                    row.createCell(6).setCellValue(issueObj.getString("created_at"));
+                    row.createCell(7).setCellValue(issueObj.getString("updated_at"));
+                    if(issueObj.has("closed_at") && !issueObj.isNull("closed_at")){
+                        row.createCell(8).setCellValue(issueObj.getString("closed_at"));
+                    }
+                }
+                String exportPath = configuration.get("EXPORT_PATH");
+                FileOutputStream fileOut = new FileOutputStream(exportPath);
+                workbook.write(fileOut);
+                fileOut.close();
+                workbook.close();
+                logger.info("Saving issues in file completed!");
+                logger.info("You can find your file .xls in " + exportPath);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
